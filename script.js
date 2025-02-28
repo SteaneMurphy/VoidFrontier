71e7fee290086d77f5b2c3393118b3b8e0570a38
James <james@steanemurphy.com>
2025-02-28 22:04:54 +1100

    CRUD: registerAccount, getSession, getCurrentWaypoint; setup environment files

---
+++ script.js
@@ -0,0 +1,74 @@
+import { CONFIG } from "./config.js";
+
+/*
+    Registers a new account token for access to the API. Only needs to be run once per account
+    and returns an session token that can be used when accessing the API endpoints
+*/
+function registerAgent()
+{
+    const URL = `https://api.spacetraders.io/v2/register`
+
+    fetch(URL, 
+        { 
+            method: "POST", 
+            headers: { 
+                "Authorization": "Bearer " + CONFIG.ACCOUNT_TOKEN,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                symbol: "STEANE",
+                faction: "ASTRO"
+            })
+        })
+    .then(response => {
+        if(response.status === 409) 
+        {
+            console.log("Agent already exists");
+            return null;
+        }
+            return response.json();
+        })
+    .then(data => console.log("Success:", data))
+    .catch(error => console.error("Request failed:", error.message));
+}
+
+/*
+    Returns information about the user (agent)
+*/
+function getAgent()
+{
+    const URL = `https://api.spacetraders.io/v2/my/agent`
+    
+    fetch(URL, 
+        {
+            method: "GET",
+            headers: 
+                {
+                    "Authorization": "Bearer " + CONFIG.AGENT_TOKEN
+                }
+        })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error.message));
+}
+
+function getCurrentWaypoint()
+{
+    const URL = `https://api.spacetraders.io/v2/systems/X1-PS39/waypoints/X1-PS39-A1`
+
+    fetch(URL, 
+        {
+            method: "GET",
+            headers: 
+                {
+                    "Authorization": "Bearer " + CONFIG.AGENT_TOKEN
+                }
+        })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error.message));
+}
+
+//registerAgent();
+getAgent();
+getCurrentWaypoint();
